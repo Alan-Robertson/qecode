f6a5e18dcdf6d7e76d592d97603cd55de0e7d0c8
Alan Robertson <arob8086@uni.sydney.edu.au>
2018-07-16 15:01:55 +1000

    Preparing code for flag fault tolerance

---
+++ error_models/gate_result.h
@@ -0,0 +1,61 @@
+#ifndef GATE_RESULT
+#define GATE_RESULT
+
+/*
+	gate_result:
+	The gate result struct, when a gate is called, it returns this object
+	:: unsigned n_qubits :: Number of qubits in the gate
+	:: error_model_f gate_error_model :: The error model to be applied when this gate is used
+	:: void* model_data :: Data for the error model
+*/
+typedef struct gate_result
+{
+	sym** gate_results;
+	double* prob_results;
+	uint32_t n_results;
+}
+
+
+// Create a new gate result object
+gate_result* gate_result_create(const uint32_t n_results)
+{
+	gate_result* gr = (gate_result*)malloc(sizeof(gate_result*));
+
+	gr->prob_results = (double*)malloc(sizeof(double) * n_results);
+
+	gr->gate_results = (sym**)malloc(sizeof(sym*) * n_results);
+
+	gr->n_results = n_results;
+	return gr;
+}
+
+
+// Create the single case
+gate_result* gate_result_create_single(double prob_result, sym* result)
+{
+	// Create our gate result object with a single entry
+	gate_result* gr = gate_result_create(1);
+
+	// Setup the 
+	gr->gate_results[0] = result;
+
+	gr->prob_results[0] = prob_result;
+
+	return gr;
+}
+
+
+// Freeing a gate result object
+void gate_result_free(gate_result* gr)
+{
+	for (uint32_t i = 0; i < gr->n_results; i++)
+	{
+		sym_free(gr->gate_results[i]);
+	}
+	free(gr->gate_results);
+	free(gr->prob_results);
+	free(gr);
+	return;
+}
+
+#endif
