0cd88266aec2f86e7497d3ae5f8259d5c05d4b26
Alan <arob8086@uni.sydney.edu.au>
2018-11-06 19:16:24 +0000

    Measurement circuit example fixed

--- examples/measurement_perror.c
+++ examples/measurement_perror.c
@@ -1,6 +1,8 @@
 //#define GATE_MULTITHREADING
 //#define N_THREADS 4
 
+#define CIRCUIT_PRINT_PROGRESS
+
 #include "../sym.h"
 #include <float.h>
 
@@ -9,6 +11,8 @@
 #include "../decoders/tailored.h"
 
 #include "../gates/clifford_generators.h"
+#include "../gates/pauli_generators.h"
+
 #include "../circuits/syndrome_measurement.h"
 #include "../circuits/circuit_search.h"
 
@@ -21,16 +25,19 @@
 
 #include "../misc/progress_bar.h"
 
-#define N_INCREMENTS 2
+#define N_INCREMENTS 1
 
 int main()
 {	
-	double init_p_error = 0.02;
-	double delta = 0.5;
- 
 	double logical_rate[N_INCREMENTS];
 
-	unsigned n_qubits = 5, n_logicals = 1, distance = 3;
+	sym* code = code_candidate_seven_c();
+	sym* logicals = code_candidate_seven_c_logicals();
+
+
+	unsigned n_qubits = code->n_qubits;
+	unsigned n_logicals = 1;
+	unsigned distance = 3;
 
 	uint32_t* preparation_targets = (uint32_t*)malloc(sizeof(uint32_t) * n_qubits);
 	for (uint32_t j = 0; j < n_qubits; j++)
@@ -38,42 +45,51 @@ int main()
 		preparation_targets[j] = j;
 	}
 
-	sym* code = code_steane();
-	sym* logicals = code_steane_logicals();
 
-	double error_rate = init_p_error;
-	double gate_error = init_p_error;	
-	progress_bar* p = progress_bar_create(N_INCREMENTS, "Progress: ");
-	for (unsigned i = 0; i < N_INCREMENTS; i++)
-	{
-		error_rate *= delta;
-		gate_error *= delta;
+	double error_rate = 0.000001;
+	double gate_error = 0.000001;
+	double bias = 1;	
+
 
 		// Initial noise
-		error_model* initial_noise = error_model_create_iid(n_qubits, error_rate);
+		error_model* initial_noise = error_model_create_iid_biased_Z(n_qubits, error_rate, bias);
 
 		// Build our circuit with noise included:
-		error_model* gate_noise = error_model_create_iid(1, gate_error);
-		error_model* cnot_noise = error_model_create_iid(2, gate_error);
+		error_model* gate_noise = error_model_create_iid_biased_X(1, gate_error, bias);
+		error_model* cnot_noise = error_model_create_iid_biased_X(2, gate_error, bias);
 		
 		// Setup the error model
-		error_model* wire_noise = error_model_create_iid(1, error_rate);
+		error_model* wire_noise = error_model_create_iid_biased_X(1, error_rate, bias);
 		
 		// The state preparation gate
-		gate* preparation = gate_create(n_qubits, gate_identity, initial_noise, NULL);
+		gate* preparation = gate_create(n_qubits, NULL, gate_noise, NULL);
 
 		// Construct the gates, including the error gate
-		gate* iid_error_gate = gate_create_iid_noise(wire_noise);
+		gate* wire = gate_create(1, NULL, wire_noise, NULL);
 		gate* cnot = gate_create(2, gate_cnot, cnot_noise, NULL);
-		gate* hadamard = gate_create(1, gate_hadamard, NULL, NULL);
-		gate* phase = gate_create(1, gate_phase, NULL, NULL);
+		gate* hadamard = gate_create(1, gate_hadamard, gate_noise, NULL);
+		gate* phase = gate_create(1, gate_phase, gate_noise, NULL);
+
+		gate* pauli_X = gate_create(1, gate_pauli_X, gate_noise, NULL);
+		gate* pauli_Z = gate_create(1, gate_pauli_Z, gate_noise, NULL);
 
 		// Our syndrome measurement circuit
 		circuit* measure = syndrome_measurement_circuit_create(code, cnot, hadamard, phase);
 
+		// Build a recovery circuit
+		// circuit* recovery = circuit_recovery_create(
+		// 	n_qubits,
+		// 	n_ancilla_qubits,
+		// 	tailored,
+		// 	pauli_X,
+		// 	pauli_Z,
+		// 	measure_ancillas);
+
 		double* initial_error_probs = error_probabilities_identity(n_qubits);
+
+
 		//double* prepared_error_probs = gate_apply(n_qubits, initial_error_probs, preparation, preparation_targets);
-		double* measured_error_probs = circuit_run(measure, initial_error_probs, NULL);
+		double* measured_error_probs = circuit_run(measure, initial_error_probs, wire);
 
 		// The error model to feed to our decoder		
 		error_model* measured_error = error_model_create_lookup(n_qubits, measured_error_probs);
@@ -84,39 +100,12 @@ int main()
 		// Find the logical correction rates
 		double* probabilities = characterise_code(code, logicals, measured_error, tailored);
 
-		logical_rate[i] = probabilities[0];
-
-		// Free allocated objects
-		error_probabilities_free(initial_error_probs);
-		//error_probabilities_free(prepared_error_probs);
-		error_probabilities_free(measured_error_probs);
-		error_probabilities_free(probabilities);
-
-		error_model_free(initial_noise);
-		error_model_free(wire_noise);
-		error_model_free(gate_noise);
-		error_model_free(cnot_noise);
-		error_model_free(measured_error);
-
-		gate_free(hadamard);
-		gate_free(cnot);
-		gate_free(phase);
-		gate_free(iid_error_gate);
-
-		decoder_free(tailored);
-
-		circuit_free(measure);
-
-		progress_bar_update(p);
-	}
-	progress_bar_free(p);
+		logical_rate[0] = probabilities[0];
 
 	printf("Physical Rate \t Logical Rate\n");
-	error_rate = init_p_error;
 	printf("((");
 	for (unsigned i = 0; i < N_INCREMENTS; i++)
 	{
-		error_rate *= delta;
 		printf("(%e, %e)", error_rate, logical_rate[i]);
 		if (i < N_INCREMENTS - 1)
 		{
